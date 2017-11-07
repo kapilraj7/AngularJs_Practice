b501f8372f87db8c8cf1c516cdf807b170e86c9a
kapilraj7 <32512573+kapilraj7@users.noreply.github.com>
2017-11-07 15:53:48 +0000

    Add files via upload

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
+// Code goes here 
+
 var app = angular.module('myApp', []);
 
 var MainController = function($scope, $http) {
-  $http.get("customers.php").then(function (response) {
-      $scope.myData = response.data.records;
+  $http.get("customers.php").then(function(response) {
+    $scope.names = response.data.records;
   });
 }
 
 app.controller('customersCtrl', MainController);
-
